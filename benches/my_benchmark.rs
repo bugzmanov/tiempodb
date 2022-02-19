69789796ede33884cac20d688a66d7801b5661c9
bugzmanov <bugzmanov@gmail.com>
2022-02-19 10:08:13 -0500

    query engine bench

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,8 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::rc::Rc;
+use parking_lot::RwLock;
+use std::collections::HashMap;
 use std::sync::Arc;
 use tiempodb::ingest::Engine;
 use tiempodb::storage;
+use tiempodb::storage::DataPoint;
+use tiempodb::storage::StorageWriter;
 
 pub fn ingest_benchmark(c: &mut Criterion) {
     let file = tempfile::NamedTempFile::new().unwrap();
@@ -18,7 +21,7 @@ pub fn ingest_benchmark(c: &mut Criterion) {
         data.push(line_str);
     }
     let mut idx = 0;
-    let mut group = c.benchmark_group("tiempodb engine");
+    let mut group = c.benchmark_group("tiempodb ingest engine");
     // Configure Criterion.rs to detect smaller differences and increase sample size to improve
     // precision and counteract the resulting noise.
     group.significance_level(0.02).sample_size(3000);
@@ -35,5 +38,31 @@ pub fn ingest_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, ingest_benchmark);
+pub fn query_engine_bench(c: &mut Criterion) {
+    let snapshot = Arc::new(RwLock::new(HashMap::default()));
+    let mut write = snapshot.write();
+    (*write).add_bulk(&vec![
+        DataPoint::new(Arc::from("table1:metric1"), 100u64, 10i64),
+        DataPoint::new(Arc::from("table1:metric1"), 101u64, 12i64),
+    ]);
+
+    drop(write);
+
+    let engine = tiempodb::sql::query_engine::QueryEngine::new(snapshot.clone());
+
+    let mut group = c.benchmark_group("tiempodb query engine");
+
+    group.significance_level(0.02).sample_size(3000);
+    group.bench_function("engine ingest", |b| {
+        b.iter(|| {
+            black_box(engine.run_query(
+                "SELECT \"metric1\", \"metric2\" FROM \"table1\" WHERE \"host\"=\"localhost\"",
+            ).unwrap());
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, ingest_benchmark, query_engine_bench);
 criterion_main!(benches);
